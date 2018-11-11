5900521a3b91199d973f2ca240d88ae3a6498d83
qinghon <wushengshijie@outlook.com>
2018-11-11 20:46:10 +0800

    nothing do it

--- aarch32-openwrt/bonuscloudinstall.sh
+++ aarch32-openwrt/bonuscloudinstall.sh
@@ -6,7 +6,7 @@ MIPSEL="https://github.com/hikaruchang/BonusCloud-Node/raw/master/openwrt-ipk/bo
 
 opkg_init(){
 	opkg update
-	opkg install curl ca-bundle ca-certificates liblzo 
+	opkg install curl wget ca-bundle ca-certificates liblzo 
 }
 
 arm_ins(){
@@ -28,29 +28,47 @@ mipsel_ins(){
 	opkg install /tmp/bonuscloudmipsel.ipk
 }
 
-
-cpu=`uname -m`
-if [[ -n ""$cpu"|grep arm" ]]; then
-	#statements
-	echo -e " the cpu is\033[31m $cpu\033[0m ,install arm"
-	arm_ins
-elif [[ -n ""$cpu"|grep mips" ]]; then
-	
-	judge_k2p=`uname -n |grep K2P`
-	if [[ -n "$judge_k2p" ]]; then
+defult_ins(){
+	echo -e " \033[31m 注意少部分路由器如K2P,新路由3会判断失败，请手动加上参数执行\033[0m "
+	echo "比如\n bonuscloudinstall.sh mipsel \n"
+	echo "装错了不要紧，卸载后重新来就好"
+	cpu=`uname -m`
+	if [[ -n ""$cpu"|grep arm" ]]; then
+		#statements
+		echo -e " the cpu is\033[31m $cpu\033[0m ,install arm"
+		arm_ins
+	elif [[ -n ""$cpu"|grep mips" ]]; then
 		
-		echo "device is K2P ,install mipsel"
-		echo "K2P can not install for now,because have more problem ,but I try it"
-        mipsel_ins
+		judge_k2p=`uname -n |grep K2P`
+		if [[ -n "$judge_k2p" ]]; then
+			
+			echo "device is K2P ,install mipsel"
+			echo "K2P can not install for now,because have more problem ,but I try it"
+	        mipsel_ins
 
+		else
+			echo -e " the cpu is\033[31m $cpu\033[0m ,install mips"
+			mips_ins
+		fi
+	elif [[ -n ""$cpu"|grep mipsel" ]]; then
+		mipsel_ins
 	else
-		echo -e " the cpu is\033[31m $cpu\033[0m ,install mips"
-		mips_ins
+		echo "you device can not install the package"
 	fi
-elif [[ -n ""$cpu"|grep mipsel" ]]; then
-	mipsel_ins
-else
-	echo "you device can not install the package"
-fi
+}
 
+case $* in
+	mips )
+		mips_ins
+		;;
+	mipsel )
+		mipsel_ins
+		;;
+	arm )
+		arm_ins
+		;;
+	* )
+		defult_ins
+		;;
 
+esac
