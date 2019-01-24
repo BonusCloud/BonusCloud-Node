f0176ed43c91511a8c879d1f0046c5701d9f265d
qinghon <wushengshijie@outlook.com>
2019-01-24 17:40:33 +0800

    mirrors bug fixed

--- aarch64/install.sh
+++ aarch64/install.sh
@@ -233,7 +233,7 @@ ins_k8s(){
         cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-aarch64/
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
