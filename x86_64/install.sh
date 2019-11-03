834bdb034cf4e9b15c2c6e23b6c441c42adbddee
sid wang <xiaoyaobilang@gmail.com>
2019-11-03 18:00:53 +0800

    Update install.sh

--- x86_64/install.sh
+++ x86_64/install.sh
@@ -42,7 +42,7 @@ support_os=(
 )
 mirror_pods=(
     "https://raw.githubusercontent.com/BonusCloud/BonusCloud-Node/master"
-    "https://git.code.tencent.com/BonusCloud/BonusCloud-Node/raw/master"
+    "https://bonuscloud-node.s3.cn-north-1.jdcloud-oss.com"
 )
 
 
